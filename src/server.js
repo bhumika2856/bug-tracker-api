10156eb50330f3efda25bb605088a48ea0f1863c
bhumika2856 <bhumikakundra28@gmail.com>
2026-06-18 21:43:14 +0530

    Added GET bugs endpoint

--- src/server.js
+++ src/server.js
@@ -2,9 +2,28 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const bugs= [
+  {
+    id:1,
+    title:"Login pgase crashes",
+    status:"Open"
+  },
+  {
+    id:2,
+    title: "Profile image not loading",
+    status:"In Progress"
+  }
+];
+
 app.get("/", (req, res) => {
   res.send("Bug Tracker API Running");
 });
+
+app.get("/bugs", (req,res) =>{
+  res.json(bugs);
+})
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
