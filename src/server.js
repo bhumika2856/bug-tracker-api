c9440fa7b85231aba67882db24f02a8380ea388e
bhumika2856 <bhumikakundra28@gmail.com>
2026-06-20 23:46:20 +0530

    Add get bug by id endpoint

--- src/server.js
+++ src/server.js
@@ -25,6 +25,31 @@ app.get("/bugs", (req,res) =>{
   res.json(bugs);
 })
 
+app.get("/bugs/:id", (req, res) => {
+  const bugId = parseInt(req.params.id);
+
+  const bug = bugs.find((b) => b.id === bugId);
+
+  if (!bug) {
+    return res.status(404).json({
+      message: "Bug not found"
+    });
+  }
+
+  res.json(bug);
+});
+
+app.post("/bugs", (req,res)=>{
+  const newBug={
+    id:bugs.length+1,
+    title:req.body.title,
+    status:"Open"
+  };
+
+  bugs.push(newBug);
+  res.status(201).json(newBug);
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
