d8e744adf9b94f7faee89255f9b65c508507105b
bhumika2856 <bhumikakundra28@gmail.com>
2026-07-04 03:25:09 +0530

    Implemented in-memory CRUD for bugs

---
+++ src/controllers/bugControllers.js
@@ -0,0 +1,79 @@
+const bugs = [
+    {
+        id: 1,
+        title: "Login page crashes",
+        status: "Open",
+        priority: "High",
+        createdAt: new Date()
+    },
+    {
+        id: 2,
+        title: "Profile image not loading",
+        status: "In Progress",
+        priority: "Medium",
+        createdAt: new Date()
+    }
+];
+
+const getAllBugs=(req,res)=>{
+    const {status}=req.query;
+
+    if(status){
+        const filteredBugs=bugs.filter(
+            (bug)=>
+                bug.status.toLowerCase()===status.toLowerCase()
+        );
+        return res.json(filteredBugs);
+    }
+    res.json(bugs);
+};
+
+const getBugById=(req,res)=>{
+    const bugId=parseInt(req.params.id);
+
+    const bug=bugs.find((b)=> b.id===bugId);
+
+    if(!bug){
+        return res.status(404).json({
+            message:"Bug not found :("
+        });
+    }
+    res.json(bug);
+};
+
+const createBug=(req,res)=>{
+    const newBug={
+        id: bugs.length+1,
+        title: req.body.title,
+        status: "Open",
+        priority: req.body.priority || "Medium",
+        createdAt: new Date()
+    };
+    bugs.push(newBug);
+
+    res.status(201).json(newBug);
+};
+
+const updateBug=(req,res)=>{
+    const bugId=parseInt(req.params.id);
+
+    const bug=bugs.find((b)=> b.id===bugId);
+
+    if(!bug){
+        return res.status(404).json({
+            message:"Bug not found :("
+        });
+    }
+    bug.title=req.body.title || bug.title;
+    bug.status=req.body.status || bug.status;
+    bug.priority=req.body.priority || bug.priority;
+
+    res.json(bug);
+};
+
+module.exports={
+    getAllBugs,
+    getBugById,
+    createBug,
+    updateBug
+};
