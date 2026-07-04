cecaf8f87115c4fe1d76aaea05657c441c79e9d5
bhumika2856 <bhumikakundra28@gmail.com>
2026-07-05 03:26:16 +0530

    Implemented JWT authentication

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express=require("express");
 const bugRoutes=require("./routes/bugRoutes");
 const errorHandler=require("./middleware/errorHandler");
+const authRoutes=require("./routes/authRoutes");
 
 const app=express();
 
 app.use(express.json());
 app.use("/bugs", bugRoutes);
-
+app.use("/auth", authRoutes);
 app.use(errorHandler);
 
 module.exports= app;
---
+++ src/controllers/authController.js
@@ -0,0 +1,70 @@
+const bycrypt= require("bcryptjs");
+const jwt=require("jsonwebtoken");
+const User=require("../models/User");
+const asyncHandler=require("../middleware/asyncHandler");
+
+const generateToken=(id)=>{
+    return jwt.sign(
+        {id},
+        process.env.JWT_SECRET,
+        {
+            expiresIn:"7d"
+        }
+    );
+};
+
+const registerUser= asyncHandler(async(req,res)=>{
+    const {name,email,password}=req.body;
+    const existingUser= await User.findOne({email});
+    if(existingUser){
+        res.status(400);
+        throw new Error("user already exists");
+    }
+    const hashedPassword= await bycrypt.hash(password,10);
+
+    const user=await User.create({
+        name,
+        email,
+        password:hashedPassword
+    });
+    res.status(201).json({
+        message:"User registered successfully",
+        token: generateToken(user._id),
+        user:{
+            id:user._id,
+            name:user.name,
+            email:user.email
+        }
+    });
+});
+
+const loginUser=asyncHandler(async(req,res)=>{
+    const {email,password}=req.body;
+
+    const user=await User.findOne({email});
+
+    if(!user){
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+    const isMatch=await bycrypt.compare(password, user.password);
+
+    if (!isMatch){
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+    res.json({
+        message:"Login successful",
+        token: generateToken(user._id),
+        user:{
+            id:user._id,
+            name: user.name,
+            email:user.email
+        }
+    });
+});
+
+module.exports={
+    registerUser,
+    loginUser
+};
