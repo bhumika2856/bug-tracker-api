e3b4f1e23a7afad1b6696879a7ca3b2afbc25351
bhumika2856 <bhumikakundra28@gmail.com>
2026-07-08 03:02:48 +0530

    Add validation for authentication routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,8 +4,25 @@ const {
     loginUser
 } =require("../controllers/authController");
 
+const {
+    registerValidator,
+    loginValidator
+} = require("../validators/authValidator");
+
+const validate = require("../middleware/validate");
+
 const router=express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post(
+    "/register",
+    registerValidator,
+    validate,
+    registerUser);
+
+router.post(
+    "/login",
+    loginValidator,
+    validate,
+    loginUser);
+
 module.exports=router;
