bf9054c23b2cd4bd48f88ef8f50f8972b7ac016f
bhumika2856 <bhumikakundra28@gmail.com>
2026-07-08 21:12:53 +0530

    Integrated Gemini AI for bug summary generation

--- src/services/ai/summaryService.js
+++ src/services/ai/summaryService.js
@@ -1,7 +1,24 @@
-const generateSummary= async ( description)=>{
-    return description;
+const axios = require("axios");
+
+const generateSummary = async (description) => {
+    const response = await axios.post(
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+        {
+            contents: [
+                {
+                    parts: [
+                        {
+                            text: `Summarize the following software bug in two concise sentence. Do not add extra explanation.\n\nBug Description:\n${description}`
+                        }
+                    ]
+                }
+            ]
+        }
+    );
+
+    return response.data.candidates[0].content.parts[0].text.trim();
 };
 
-module.exports={
+module.exports = {
     generateSummary
 };
