78877a1f5d28f2149a4649c479dd588ba29c0bb0
bhumika2856 <bhumikakundra28@gmail.com>
2026-07-15 03:06:01 +0530

    feat(backend): enable CORS for frontend integration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express=require("express");
+const cors = require("cors");
 const bugRoutes=require("./routes/bugRoutes");
 const errorHandler=require("./middleware/errorHandler");
 const authRoutes=require("./routes/authRoutes");
 
 const app=express();
-
+app.use(cors());
 app.use(express.json());
 app.use("/bugs", bugRoutes);
 app.use("/auth", authRoutes);
