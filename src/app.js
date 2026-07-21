729f37c78304cbf1c5bdb304427f1726a2d74be8
bhumika2856 <bhumikakundra28@gmail.com>
2026-07-22 02:45:52 +0530

    Restore CORS configuration

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
-const express=require("express");
+const express = require("express");
 const cors = require("cors");
-const bugRoutes=require("./routes/bugRoutes");
-const errorHandler=require("./middleware/errorHandler");
-const authRoutes=require("./routes/authRoutes");
 
-const app=express();
+const bugRoutes = require("./routes/bugRoutes");
+const errorHandler = require("./middleware/errorHandler");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 app.use("/bugs", bugRoutes);
 app.use("/auth", authRoutes);
+
 app.use(errorHandler);
 
-module.exports= app;
+module.exports = app;
